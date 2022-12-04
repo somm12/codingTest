7ea1581712c72eec91c01211f1c40789bad61ef9
somm12 <sominan8744@gmail.com>
2022-12-05 07:32:39 +0900

    이코테 음료수 얼려먹기.py update

---
+++ 3주차/1205_이코테_음료수얼려먹기.py
@@ -0,0 +1,23 @@
+n, m = map(int, input().split())
+arr = []
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+for _ in range(n):
+    arr.append(list(input()))
+def dfs(x, y):
+    arr[x][y] = '1'
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if nx < 0 or ny < 0 or nx >= n or ny >= m:
+            continue
+        else:
+            if arr[nx][ny] == '0':
+                dfs(nx, ny)
+cnt = 0
+for i in range(n):
+    for j in range(m):
+        if arr[i][j] == '0':
+            dfs(i,j)
+            cnt += 1
+print(cnt)
