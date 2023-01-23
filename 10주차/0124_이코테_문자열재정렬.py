5ad5c074ec5cf6805c160c085b7506ea97fb13bf
somm12 <sominan8744@gmail.com>
2023-01-24 07:26:11 +0900

    이코테 문자열재정렬.py

--- 10주차/0124_이코테_문자열재정렬.py
+++ 10주차/0124_이코테_문자열재정렬.py
@@ -9,3 +9,20 @@ for c in s:
         alp += c
 
 print(alp + str(res))
+
+# 다른 풀이. ** sort는 배열만 가능, join을 통해 배열을 문자열로 변환 가능
+data = input()
+result = []# 문자를 담을 배열
+value = 0# 각 숫자를 더할 변수
+
+for x in data:
+    if x.isalpha():
+        result.append(x)
+    else:
+        value += int(x)
+result.sort()
+
+if value != 0:
+    result.append(str(value))
+
+print(''.join(result))
