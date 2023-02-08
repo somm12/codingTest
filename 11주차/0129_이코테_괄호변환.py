1eb1f5e981d1999a8b04c5ae4a1adf77b8834549
somm12 <sominan8744@gmail.com>
2023-02-08 11:20:08 +0900

    감시하기 다시풀기 update

--- 11주차/0129_이코테_괄호변환.py
+++ 11주차/0129_이코테_괄호변환.py
@@ -1,50 +1,37 @@
-def check(s):
-    a = []
-    for i in range(len(s)):
-        if len(a) != 0:
-            if a[-1] == '(' and s[i] == ')':
-                a.pop()
-            else:
-                a.append(s[i])
-        else:
-            a.append(s[i])
-    if len(a) > 0:
-        return False
-    else:
-        return True
-
-def divide(s):
-    a = 0
-    b = 0
-    for i in range(len(s)):
-        if s[i] == '(':
-            a += 1
-        else:
-            b += 1
-        if a == b:
-            u = s[:i+1]
-            v = s[i+1:]
-            return [u,v]
-def sol(p):
-    ans = ''
-    temp = ''
-    if len(p) == 0:
-        return ans
-    if check(p):
-        return p
-    u,v = divide(p)
-    while check(u):
-        ans += u
-        u, v = divide(v)
-    temp = '(' + v + ')'
-    u = u[1:len(u)-1]
-    t = ''
-    for i in u:
+def check(w):
+    cnt = 0
+    for i in w:
         if i == '(':
-            t += ')'
+            cnt += 1
         else:
-            t += '('
-    ans += temp + t
-    return ans
-
-print(sol("(()())()"))
+            if cnt == 0:
+                return False
+            cnt -= 1
+    return True
+def divide(w):
+    cnt = 0
+    for i in range(len(w)):
+        if w[i] == '(':
+            cnt += 1
+        else:
+            cnt -= 1
+        if cnt == 0:
+            return [w[:i+1],w[i+1:]]
+def solution(p):
+    answer = ''
+    if p == '':
+        return ''
+    u,v = divide(p)
+    if check(u):
+        answer = u + solution(v)
+    else:
+        answer = '('
+        answer += solution(v) + ')'
+        u = list(u[1:-1])
+        for i in range(len(u)):
+            if u[i] == '(':
+                u[i] = ')'
+            else:
+                u[i] = '('
+        answer += ''.join(u)
+    return answer
--- 11주차/0130_이코테_감시하기.py
+++ 11주차/0130_이코테_감시하기.py
@@ -1,65 +1,43 @@
 from itertools import combinations
-
+from copy import deepcopy
 n = int(input())
-board = []
-teachers = []
-spaces = []
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
 
+g = []
+arr = []
 for i in range(n):
-    board.append(list(input().split()))
+    g.append(list(input().split()))
     for j in range(n):
-        if board[i][j] == 'T':
-            teachers.append((i,j))
-        if board[i][j] == 'X':
-            spaces.append((i,j))
-
-def watch(x,y,direction):
-    if direction == 0:
-        while y >= 0:
-            if board[x][y] == 'S':
-                return True
-            if board[x][y] == 'O':
-                return False
-            y -= 1
-    if direction == 1:
-        while y < n:
-            if board[x][y] == 'S':
-                return True
-            if board[x][y] == 'O':
-                return False
-            y += 1
-    if direction == 2:
-        while x < n:
-            if board[x][y] == 'S':
-                return True
-            if board[x][y] == 'O':
-                return False
-            x += 1
-    if direction == 3:
-        while x >= 0:
-            if board[x][y] == 'S':
-                return True
-            if board[x][y] == 'O':
-                return False
-            x -= 1
+        if g[i][j] == 'X':
+            arr.append((i,j))
+def dfs(x,y,d):
+    nx = x + dx[d]
+    ny = y + dy[d]
+    
+    while 0 <= nx < n and 0 <= ny < n and t[nx][ny] != 'O':
+        if t[nx][ny] == 'S':
+            return True
+        else:
+            nx += dx[d]
+            ny += dy[d]
     return False
+        
+res = 'NO'
 def process():
-    for x, y in teachers:
-        for i in range(4):
-            if watch(x,y,i):
-                return True
+    for i in range(n):
+        for j in range(n):
+            if t[i][j] == 'T':
+                for k in range(4):
+                    if dfs(i,j,k):
+                        return True
     return False
-ans = False
-for c in combinations(spaces,3):
-    for x, y in c:
-        board[x][y] = 'O'
-    if not process():# 발견 못했다면,
-        ans = True
-        break
-    for x,y in c:
-        board[x][y] = 'X'
 
-if ans:
-    print("YES")
-else:
-    print("NO")
+for obstacle in combinations(arr,3):
+    t = deepcopy(g)
+    for i,j in obstacle:
+        t[i][j] = 'O'
+    if not process():
+        res = 'YES'
+        break
+print(res)
