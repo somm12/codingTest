415a5818b4e43bbbe10d6cf0e6c4385073eaf464
somm12 <sominan8744@gmail.com>
2023-02-17 07:35:56 +0900

    외벽점검

---
+++ 13주차/0217_이코테_외벽점검.py
@@ -0,0 +1,24 @@
+from itertools import permutations
+
+
+def solution(n,weak,dist):
+    result = len(dist) + 1
+    length = len(weak)
+    for i in range(length):
+        weak.append(weak[i]+n)
+        
+    for start in range(length):
+        for friends in list(permutations(dist, len(dist))):
+            count = 1
+            position = weak[start] + friends[count-1]
+            for i in range(start, start+length):
+                if position < weak[i]:
+                    count += 1
+                    if len(dist) < count:
+                        break
+                    position = weak[i] + friends[count-1]
+            result = min(result,count)
+    if result > len(dist):
+        return -1
+    return result
+print(solution(12,[1,3,4,9,10],[3,5,7]))
