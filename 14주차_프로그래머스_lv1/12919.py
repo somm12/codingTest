1029ee4926ebf3c0b5dcdbe32b7bcc8026cfa63f
somm12 <sominan8744@gmail.com>
2023-02-21 11:07:33 +0900

    프로그래머스 12919.py update

--- 14주차_프로그래머스_lv1/12919.py
+++ 14주차_프로그래머스_lv1/12919.py
@@ -2,7 +2,6 @@ def solution(seoul):
     answer = ''
     for i in range(len(seoul)):
         if seoul[i] == 'Kim':
-            
             answer = "김서방은 "+str(i)+"에 있다"
             break
     return answer
