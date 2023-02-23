80da06717cdfca32eb11de91f0d79dade4950103
somm12 <sominan8744@gmail.com>
2023-02-23 09:09:08 +0900

    연산자끼워넣기 라이브러리 없이 풀기

--- 11주차/0130_이코테_연산자끼워넣기.py
+++ 11주차/0130_이코테_연산자끼워넣기.py
@@ -1,42 +1,34 @@
-from itertools import permutations
-import sys
-input = sys.stdin.readline
-
 n = int(input())
-arr = list(map(int, input().split()))
-a = list(map(int, input().split()))
-op = []
-for i in range(4):
-    if i == 0:
-        for _ in range(a[i]):
-            op.append('+')
-    elif i == 1:
-        for _ in range(a[i]):
-            op.append('-')
-    elif i == 2:
-        for _ in range(a[i]):
-            op.append('x')
-    else:
-        for _ in range(a[i]):
-            op.append('//')
-maxV = (10**9) * -1
-minV = (10**9) 
 
-for ops in list(set(permutations(op,n-1))):
-    temp = arr[0]
-    for i in range(n-1):
-        if ops[i] == '+':
-            temp += arr[i+1]
-        elif ops[i] == '-':
-            temp -= arr[i+1]
-        elif ops[i] == 'x':
-            temp *= arr[i+1]
-        else:
-            if temp < 0:
-                temp = (abs(temp)//arr[i+1])* -1
-            else:
-                temp //= arr[i+1]
-    maxV = max(maxV, temp)
-    minV = min(minV, temp)
-print(maxV)
-print(minV)
+data = list(map(int,input().split()))
+
+add,sub,mul,div = map(int,input().split())
+
+min_value = 1e9
+max_value = -1e9
+
+def dfs(i,now):
+    global min_value, max_value, add, sub, mul, div
+    if i == n:
+        min_value = min(min_value,now)
+        max_value = max(max_value, now)
+    else:
+        if add > 0:
+            add -= 1
+            dfs(i+1, now + data[i])
+            add += 1
+        if sub > 0 :
+            sub -= 1
+            dfs(i+1, now - data[i])
+            sub += 1
+        if mul > 0:
+            mul -= 1
+            dfs(i+1, now * data[i])
+            mul += 1
+        if div > 0:
+            div -= 1
+            dfs(i+1, int(now / data[i]))
+            div += 1
+dfs(1,data[0])
+print(max_value)
+print(min_value)
