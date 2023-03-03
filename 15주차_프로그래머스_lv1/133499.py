aee921cd4fb79b563fc575ff912cc691e3fdc698
“somm12” <sominan8744@gmail.com>
2023-03-03 10:22:50 +0900

    옹알이2

---
+++ 15주차_프로그래머스_lv1/133499.py
@@ -0,0 +1,19 @@
+from itertools import permutations
+def solution(babbling):
+    answer = 0
+    dict = {'aya': 'A', 'ye': 'B', 'woo': 'C', 'ma':'D'}
+    for i in range(len(babbling)):
+        for j in dict:
+            if j in babbling[i]:
+                babbling[i] = babbling[i].replace(j, dict[j])
+    for s in babbling:
+        for j in range(len(s)):
+            if s[j] not in ('A','B','C','D'):
+                break
+            if j> 0:
+                if s[j] == s[j-1]:
+                    break
+        else:
+            answer += 1
+    return answer
+# 옹알이
