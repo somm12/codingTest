4419db26eb63327a07ec93629c50ecd4158ccaab
“somm12” <sominan8744@gmail.com>
2023-03-07 07:18:22 +0900

    대충 만든 자판

---
+++ 16주차_프로그래머스_lv1,2/160586.py
@@ -0,0 +1,24 @@
+def solution(keymap, targets):
+    answer = []
+    dict = {}
+    for key in keymap:
+        for i in range(len(key)):
+            if key[i] in dict:
+                if dict[key[i]] > i+1:
+                    dict[key[i]] = i+1
+            else:
+                dict[key[i]] = i+1
+    for t in targets:
+        cnt = 0
+        for i in range(len(t)):
+            if t[i] in dict:
+                cnt += dict[t[i]]
+            else:
+                answer.append(-1)
+                break
+        else:
+            answer.append(cnt)    
+           
+            
+    return answer
+# 대충 만든 자판
