df8270731aae9b5c57a193bae39148991ef3c1a4
“somm12” <sominan8744@gmail.com>
2023-03-10 07:51:40 +0900

    숫자의 표현 풀이2 update

--- 16주차_프로그래머스_lv1,2/0310_lv2_12924.py
+++ 16주차_프로그래머스_lv1,2/0310_lv2_12924.py
@@ -6,7 +6,6 @@ def solution(n):
     while s <= e:
         if total <= n:
             if total == n:
-                print(s,e)
                 answer += 1
             e += 1
             total += e
@@ -17,3 +16,21 @@ def solution(n):
 # 숫자의 표현
 # 범위는 자연수 부터 10,000 이므로
 # s는 0부터 시작해야 n == 1일 경우를 포함.
+
+# 아래는 투 포인터 다른 풀이
+def solution(n):
+    target = n
+    cnt = 0
+    total= 0
+    e = 1
+    for s in range(1,n+1):
+        while total < target and e <= n:
+            total += e
+            e += 1
+        if total == target:
+            cnt += 1
+        total -= s
+        
+    
+    
+    return cnt
