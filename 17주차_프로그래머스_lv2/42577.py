363958c10a6908f3f709ea999a61055848a46f93
“somm12” <sominan8744@gmail.com>
2023-03-17 11:11:16 +0900

    전화번호 목ㄹㄱ update

---
+++ 17주차_프로그래머스_lv2/42577.py
@@ -0,0 +1,16 @@
+def solution(phone_book):
+    answer = True
+    dict = {}
+    phone_book.sort(key=len)ㅁ
+    for i in phone_book:
+        if len(dict) == 0:
+            dict[i] = 1
+        else:
+            tmp = ''
+            for s in i:
+                tmp += s
+                if tmp in dict:
+                    return False
+            dict[i] = 1
+    return answer
+# 전화번호 목록
