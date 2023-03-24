0b1d73f2c87ea6ca43b7f2fa3bf653b60041f947
“somm12” <sominan8744@gmail.com>
2023-03-25 07:40:11 +0900

    소수찾기

--- 18주차_lv2/42839.py
+++ 18주차_lv2/42839.py
@@ -33,3 +33,26 @@ def solution(numbers):
     dfs(0,res,ch,numbers)
     return len(dict)
 # 소수찾기
+# 아래는 permutations 이용한 풀이. 중복과 0이 첫번째 자리에 올 경우 생기는 중복에 조심**
+dict = {}
+from itertools import permutations
+def isPrime(n):
+    if n < 2:
+        return False
+    if n == 2:
+        return True
+    for i in range(2, int(n**0.5) +1):
+        if n % i == 0:
+            return False
+    return True
+
+    
+def solution(numbers):
+    a = set()
+    for i in range(1,len(numbers)+1):
+        for i in list(permutations(list(numbers),i)):
+            i = int(''.join(i))
+            if isPrime(i):
+                a.add(i)
+    
+    return len(a)
