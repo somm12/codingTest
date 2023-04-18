9c2dad12fb90b34f6293db0f1078c57d6b930a64
“somm12” <sominan8744@gmail.com>
2023-04-19 07:22:41 +0900

    simplified 미로 탈출.py

--- 22주차/미로탈출.py
+++ 22주차/미로탈출.py
@@ -54,6 +54,7 @@ def solution(maps):
                 s = [i,j]
             elif maps[i][j] == 'L':
                 l = [i,j]
+            
           
     d1 = lever(maps,s)
     if d1 == -1:
@@ -61,6 +62,58 @@ def solution(maps):
     answer += d1
     
     d2 = door(maps,l)
+    if d2 == -1:
+        return -1
+    answer += d2
+    return answer
+
+# 함수 하나로 정의하여 코드 수 줄이기
+from collections import deque
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+def move(maps,start,end):
+    n = len(maps)
+    m = len(maps[0])
+    x,y = start[0],start[1]
+    endx, endy = end[0],end[1]
+    check = [[0]*m for _ in range(n)]
+    q = deque()
+    check[x][y] = 1
+    q.append((x,y,0))
+    while q:
+        x,y,dist = q.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny <m and not check[nx][ny]:
+                if maps[nx][ny] != 'X':
+                    check[nx][ny] = 1
+                    if (nx,ny) == (endx,endy):
+                        return dist + 1
+                    q.append((nx,ny,dist+1))
+    return -1
+ 
+
+def solution(maps):
+    answer = 0
+    s = []
+    l = []
+    e = []
+    for i in range(len(maps)):
+        for j in range(len(maps[0])):
+            if maps[i][j] == 'S':
+                s = [i,j]
+            elif maps[i][j] == 'L':
+                l = [i,j]
+            elif maps[i][j] == 'E':
+                e = [i,j]
+          
+    d1 = move(maps,s,l)
+    if d1 == -1:
+        return -1
+    answer += d1
+    
+    d2 = move(maps,l,e)
     if d2 == -1:
         return -1
     answer += d2
