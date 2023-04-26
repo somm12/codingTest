ef6d7cef983290acc4728e4e1901dffbabd81ffb
“somm12” <sominan8744@gmail.com>
2023-04-26 10:05:32 +0900

    외판원순회2 comment added

--- 23주차/외판원순회2.py
+++ 23주차/외판원순회2.py
@@ -1,15 +1,18 @@
 
 def dfs(L, res,total,now):
     global ans
+    # 경로 비용이 이미 현재까지의 최솟값보다 많아 졌다면 return
     if total > ans:
         return
     if L == n:
+        # 마지막 지점에서 출발지점으로 가는 경로가 존재할 경우.
         if w[res[n-1]][res[0]] != 0:
             total += w[res[n-1]][res[0]]
             ans= min(ans,total)
         return
     
     for i in range(n):
+        # 다음 지점 경로가 존재하는지 확인 필수.
         if L == 0 or (not visited[i] and w[res[L-1]][i] != 0):
             visited[i] = 1
             dfs(L+1,res+[i],total+w[now][i],i)
