17f1a52b7abf1576a37c29fb279e42d29a53de53
somm12 <sominan8744@gmail.com>
2023-10-19 12:31:07 +0900

    피로도 .js update

---
+++ 47주차/피로도.js
@@ -0,0 +1,23 @@
+function solution(k, dungeons) {
+  var answer = -1;
+
+  let n = dungeons.length;
+  let visited = new Array(n).fill(0);
+
+  function dfs(k, cnt) {
+    //현재 남은 피로도, 최대 개수.
+    answer = Math.max(answer, cnt);
+
+    for (let i = 0; i < n; i++) {
+      if (!visited[i] && k >= dungeons[i][0]) {
+        // 남은 피로도가 필요필요도 이상이고, 방문 안했따면,
+        visited[i] = 1;
+        dfs(k - dungeons[i][1], cnt + 1);
+        visited[i] = 0;
+      }
+    }
+  }
+  dfs(k, 0);
+
+  return answer;
+}
