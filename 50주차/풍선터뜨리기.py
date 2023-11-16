2875e0b5d279cb1451858f8f5ba68a68d4ea35ac
somm12 <sominan8744@gmail.com>
2023-11-16 15:39:05 +0900

    풍선터뜨리기.py update

---
+++ 50주차/풍선터뜨리기.py
@@ -0,0 +1,20 @@
+n = int(input())
+idx = 0
+result = []
+
+data = list(map(int, input().split()))
+index = [x for x in range(1, n + 1)]
+
+temp = data.pop(idx)
+result.append(index.pop(idx))
+
+while data:
+    if temp < 0:
+        idx = (idx + temp) % len(data)
+    else:
+        idx = (idx + (temp - 1)) % len(data)
+    temp = data.pop(idx)
+    result.append(index.pop(idx))
+
+for r in result:
+    print(r, end=' ')
