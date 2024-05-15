59e53b96f5bf483f25b45b598b8dd8875bd19e62
somm12 <sominan8744@gmail.com>
2024-05-15 10:20:27 +0900

    feat: boj 2573 빙산

---
+++ 76주차/2573.js
@@ -0,0 +1,102 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const dx = [-1, 1, 0, 0];
+const dy = [0, 0, -1, 1];
+const melting = () => {
+  const tmp = Array(N) // 임시 배열 복사. 원본 배열로 계산하면, 답이 틀림.
+    .fill()
+    .map((e) => Array(M).fill(0));
+  for (let i = 0; i < N; i++) {
+    for (let j = 0; j < M; j++) {
+      tmp[i][j] = g[i][j];
+    }
+  }
+  for (let x = 1; x < N - 1; x++) {
+    for (let y = 1; y < M - 1; y++) {
+      if (g[x][y] > 0) {
+        let cnt = 0;
+        for (let i = 0; i < 4; i++) {
+          const [nx, ny] = [x + dx[i], y + dy[i]];
+          if (g[nx][ny] == 0) cnt += 1;
+        }
+        tmp[x][y] -= cnt;
+        if (tmp[x][y] < 0) tmp[x][y] = 0;
+      }
+    }
+  }
+
+  for (let i = 0; i < N; i++) {
+    // 녹고난 후 상태를 원본 배열에 업데이트.
+    for (let j = 0; j < M; j++) {
+      g[i][j] = tmp[i][j];
+    }
+  }
+};
+
+const dfs = (x, y) => {
+  visited[x][y] = 1;
+  for (let i = 0; i < 4; i++) {
+    const [nx, ny] = [x + dx[i], y + dy[i]];
+    if (!visited[nx][ny] && g[nx][ny] > 0) {
+      dfs(nx, ny);
+    }
+  }
+};
+
+const cnt = () => {
+  let cnt = 0;
+  for (let x = 1; x < N - 1; x++) {
+    for (let y = 1; y < M - 1; y++) {
+      if (g[x][y] > 0 && !visited[x][y]) {
+        dfs(x, y);
+        cnt += 1;
+      }
+    }
+  }
+  visited = Array(N)
+    .fill()
+    .map((e) => Array(M).fill(0));
+  return cnt;
+};
+const solution = () => {
+  let isExit = false;
+  let year = 0;
+  while (true) {
+    melting();
+
+    year += 1;
+    const total = cnt();
+
+    if (total >= 2) {
+      isExit = true;
+      console.log(year);
+      break;
+    } else if (total == 0) break;
+  }
+  if (!isExit) console.log(0);
+};
+
+let [N, M] = [null, null];
+let g = [];
+let l = 0;
+let visited = [];
+rl.on("line", (line) => {
+  if (!N) {
+    [N, M] = line.split(" ").map((e) => +e);
+    visited = Array(N)
+      .fill()
+      .map((e) => Array(M).fill(0));
+  } else {
+    g.push(line.split(" ").map((e) => +e));
+    l += 1;
+    if (l == N) rl.close();
+  }
+});
+rl.on("close", () => {
+  solution();
+  process.exit();
+});
